00286aea29bb8108a00303c855915834ce60ec2e
Adam Raaif Nasheed <adam.raaif.nasheed7@gmail.com>
2020-11-05 23:19:30 +0500

    Added VLC speed controls

--- commands/remote_actions.py
+++ commands/remote_actions.py
@@ -5,17 +5,33 @@ import config
 
 
 def press_key(key):
-    def press():
+    def perform():
         keyboard = Controller()
         keyboard.press(key)
         keyboard.release(key)
-    return press
+    return perform
 
 def mouse_twitch():
     mouse = MouseController()
     mouse.position = (100, 100)
     time.sleep(0.5)
     mouse.move(100, 100)
+
+def vlc_change_speed(speed):
+    def perform():
+        # Reset speed to x1.0
+        press_key('=')()
+        # Get no. of increments of +0.1
+        incr = round((float(speed) - 1) / 0.1)
+        for i in range(incr):
+            press_key(']')()
+    return perform
+
+def vlc_increment_speed():
+    press_key(']')()
+
+def vlc_decrement_speed():
+    press_key('[')()
 
 def toggle_silent_mode():
     config.SILENT_MODE = not config.SILENT_MODE
@@ -43,7 +59,14 @@ ACTIONS_LIST = {
     "vu": { "label": "🔈+", "action": press_key(Key.up), "reply": "Increasing volume ..." },
     "np": { "label": "🖱 Now playing", "action": mouse_twitch, "reply": "🖱" },
     "s": { "label": "⏭ Skip intro", "action": press_key('s'), "reply": "Skipping intro ..." },
-    "sil": { "label": "Silent mode", "action": toggle_silent_mode, "reply": "" }
+    "sil": { "label": "Silent mode", "action": toggle_silent_mode, "reply": "" },
+    "spd": { "label": "🏃‍-", "action": vlc_decrement_speed, "reply": "Decrementing playback speed by 0.1 ..." },
+    "sp1": { "label": "x1", "action": vlc_change_speed(1), "reply": "Setting playback speed to x1 ..." },
+    "sp1.5": { "label": "x1.5", "action": vlc_change_speed(1.5), "reply": "Setting playback speed to x1.5 ..." },
+    "sp2": { "label": "x2", "action": vlc_change_speed(2), "reply": "Setting playback speed to x2 ..." },
+    "sp3": { "label": "x3", "action": vlc_change_speed(3), "reply": "Setting playback speed to x3 ..." },
+    "sp4": { "label": "x4", "action": vlc_change_speed(4), "reply": "Setting playback speed to x4 ..." },
+    "spu": { "label": "🏃‍+", "action": vlc_increment_speed, "reply": "Incrementing playback speed by 0.1 ..." }
 }
 
 KEYBOARD_LAYOUT = [[get_label(button) for button in row] for row in [
@@ -51,5 +74,6 @@ KEYBOARD_LAYOUT = [[get_label(button) for button in row] for row in [
     ["rw", "p", "ff"],
     ["vd", "m", "vu"],
     ["np", "s"],
+    ["spd", "sp1", "sp1.5", "sp2", "sp3", "sp4", "spu"],
     ["sil"]
 ]]
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_VERSION = "1.2.0"
+BOT_VERSION = "1.3.0"
 SILENT_MODE = True
